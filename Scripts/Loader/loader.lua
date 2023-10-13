8a9dc4bf249c721e79df901a55b8c9276ab9cc68
FzLoader <canaldoninja1@gmail.com>
2023-10-13 00:41:51 -0300

    Update loader.lua

--- Scripts/Loader/loader.lua
+++ Scripts/Loader/loader.lua
@@ -1,7 +1,7 @@
 local games = {
-    [4442272183] = 'Blox%20Fruits'
+    [994732206] = 'Blox%20Fruits'
 }
 
-if (game.PlaceId and games[game.PlaceId]) then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/FzLua/Roblox/main/Scripts/Loader/'..games[game.PlaceId]..'/run.lua?token=GHSAT0AAAAAACIZAKZMTX7357DPN3GUHDLYZJHKW7Q'))()
+if (game.GameId and games[game.GameId]) then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/FzLua/Roblox/main/Scripts/Loader/'..games[game.GameId]..'/run.lua?token=GHSAT0AAAAAACIZAKZMTX7357DPN3GUHDLYZJHKW7Q'))()
 end
