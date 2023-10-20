8e3783a6fef6be0395c17691cc1b79f72ed3e7a7
FzLoader <canaldoninja1@gmail.com>
2023-10-20 01:07:51 -0300

    Update run.lua

--- Scripts/Loader/Blox Fruits/run.lua
+++ Scripts/Loader/Blox Fruits/run.lua
@@ -377,6 +377,7 @@ function whileStart()
         
         local pos = localPlayer.Character.HumanoidRootPart.Position
         
+        print(_G.fzLoader.cache.options['Own Functions']['Change Aura Color'].CurrentValue)
         if (_G.fzLoader.cache.options['Own Functions']['Change Aura Color'].CurrentValue) then
             if (localPlayer.Character.Humanoid.Sit) then
                 localPlayer.Character.Humanoid.Sit = false
